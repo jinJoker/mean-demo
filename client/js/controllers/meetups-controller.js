32825e1590559bc531b4fb8935959c0861276f8c
jinJoker <joker_d_j@hotmail.com>
2015-12-22 18:07:27 +0800

    49:00

--- client/js/controllers/meetups-controller.js
+++ client/js/controllers/meetups-controller.js
@@ -1,13 +1,18 @@
-angular.module("demo", [])
-    .controller("meetupsController", ['$scope', function($scope){
-        $scope.meetupsCount = 10;
+app.controller("meetupsController", ['$scope', '$resource', function($scope, $resource){
+        var Meetup = $resource('/api/meetups');
+        
         $scope.meetups = [
             { name: "MEAN SF Developers" },
             { name: "Some other meetups" }
         ];
         $scope.createMeetup = function(){
-            $scope.meetups.push({name:$scope.meetupName});
-            $scope.meetupName = '';
+            //old version: list will be refreshed by refreshing
+            //$scope.meetups.push({name:$scope.meetupName});
+            //$scope.meetupName = '';
+            
+            var meetup = new Meetup();
+            meetup.name = $scope.meetupName;
+            meetup.$save();
     }
 }]);
 
