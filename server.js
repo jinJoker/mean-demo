f04c42da56511046e82cb9296797199bbb20e7e1
jinJoker <joker_d_j@hotmail.com>
2015-12-22 18:14:43 +0800

    commit for cancel

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var meetupsController = require('./server/controllers/meetups-controller');
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
 
 app.get('/', function(req,res){
-    res.sendfile(__dirname + '/client/views/index.html');
+    res.sendFile(__dirname + '/client/views/index.html');
 });
 
 app.use('/js', express.static(__dirname + '/client/js'));
+
+app.post('/api/meetups', meetupsController.create);
 
 app.listen(3000, function(){
     console.log('I\'m listening');
